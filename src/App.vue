841458b4836eb2ed60f2903beb1bfc02e6302121
GlebGlushchenko <darkvee1n@ya.ru>
2020-09-12 23:29:03 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="todo">
+        <div class="todo__header">
+    <h4>Список задач</h4>
+</div>
+    <TaskField />
+<div class="todo__list">
+    <ListItem v-for="(task,index) in tasks" 
+     :key="index" 
+    :text="task.text" 
+    :index="index"
+    :completed="task.completed"
+    @ontogglecomplited="onToggleComplited"/>
+    
+</div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListItem from './components/ListItem.vue'
+import TaskField from './components/TaskField'
 
 export default {
   name: 'App',
+  methods:{
+    onToggleComplited(index){
+      this.tasks[index].completed =! this.tasks[index].completed
+    }
+  },
+  data:()=>({
+    tasks:[{ 
+      text:"Изучить Vue JS",
+      completed:true
+    },{
+      text:"Разработать Todo на Vue JS",
+      completed:false
+    }]
+  }),
   components: {
-    HelloWorld
+    ListItem,
+    TaskField
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/base.css';
 </style>
